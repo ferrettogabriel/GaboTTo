2e202188a0fa97dd0a50cc3acad062f0f3dc2502
ferrettogabriel <58693676+ferrettogabriel@users.noreply.github.com>
2026-01-04 21:17:32 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'  // si tenés CSS, sino borrá esta línea
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
