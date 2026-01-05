cd4b26d5974eb592540964012021336caaa9562a
ferrettogabriel <58693676+ferrettogabriel@users.noreply.github.com>
2026-01-04 21:48:21 -0300

    Update vite.config.ts
    Add vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 export default defineConfig(({ mode }) => {
   // Carga las variables de entorno para que estén disponibles
   const env = loadEnv(mode, process.cwd(), '');
