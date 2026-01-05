8ad31be06bce2e3e03dd75af59128609f877abf5
ferrettogabriel <58693676+ferrettogabriel@users.noreply.github.com>
2026-01-04 22:01:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'  // Necesario para el alias @
+import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno desde el directorio actual
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        // Alias para importar usando @/
+        '@': path.resolve(process.cwd(), './'), 
+      },
     },
-  },
-  server: {
-    port: 3000,
-    host: true,
-  },
+    define: {
+      // Compatibilidad para librerías que usen process.env
+      'process.env': JSON.stringify(env)
+    },
+    server: {
+      port: 3000,
+      host: true 
+    }
+  }
 })
